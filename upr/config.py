e236c148520a900d95d004e613506b460b0d507f
sorressean <tyler@tysdomain.com>
2016-09-11 11:24:10 -0400

    Added config  object.

---
+++ upr/config.py
@@ -0,0 +1,12 @@
+"""
+Configuration module.
+Holds all variables and extra information for the app.
+TODO: extend this to load a non-repo config file to override defaults.
+"""
+import os
+#application path.
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))    
+#enable debugging on development.
+DEBUG=True
+#define our sqlite database.
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
