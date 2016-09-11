868637e3e01aba8975dec3b1ab46cf85fcaa8921
sorressean <tyler@tysdomain.com>
2016-09-11 11:20:27 -0400

    Added a templates directory to contain required templates.

---
+++ upr/__init__.py
@@ -0,0 +1,13 @@
+"""
+Creates the UPR package and imports and initializes all required objects.
+"""
+#import top-level flask bits.
+from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
+#create the app.
+app = Flask("upr")
+#load the app's config.
+app.config.from_object('config')
+#initialize the sqlalchemy object.
+db = SQLAlchemy(app)
