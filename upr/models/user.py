ae419a51f5dd8ab33d325b110ae0dae67349ccee
Harry Staley <staleyh@gmail.com>
2016-09-17 08:30:51 -0500

    CHORE: cleaned up style to meet PEP8

--- upr/models/user.py
+++ upr/models/user.py
@@ -3,19 +3,20 @@ Includes the user model.
 """
 from upr import db
 
-    
+
 class User(db.Model):
     """
     The user model is responsible for handling all user operations.
     These include creation, search and delete.
         """
     id = db.Column(db.Integer, primary_key=True)
-    email=db.Column(db.String(128), unique=True)
+    email = db.Column(db.String(128), unique=True)
     password = db.Column(db.String(128))
 
     def __init__(self, email, password):
         self.email = email
         self.password = password
+
     def commit(self):
         """
         Commit this object to the db
