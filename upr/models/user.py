99a66fb254c512a2b93bbd89f8b653d21c681202
sorressean <tyler@tysdomain.com>
2016-09-17 11:32:38 -0400

    Added user model logic.

--- upr/models/user.py
+++ upr/models/user.py
@@ -1,8 +1,7 @@
 """
 Includes the user model.
 """
-from upr import db
-
+from upr import db, login_manager
 
 class User(db.Model):
     """
@@ -10,12 +9,15 @@ class User(db.Model):
     These include creation, search and delete.
         """
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True)
-    password = db.Column(db.String(128))
+    email = db.Column(db.String(128), unique=True, nullable = False)
+    password = db.Column(db.String(128), nullable = False)
+    is_active = db.Column(db.Boolean, nullable=False)
 
     def __init__(self, email, password):
         self.email = email
         self.password = password
+        self.is_authenticated = False
+        self.is_anonymous = False
 
     def commit(self):
         """
@@ -29,3 +31,15 @@ class User(db.Model):
         Returns the user by the specified email address.
         """
         return User.query.filter_by(email=search).first()
+    @staticmethod
+    def find_by_id(search):
+        """
+        Locates the user by id and returns it.
+        """
+        return User.query.filter_by(id=search).first()
+    def get_id(self):
+        return unicode(self.id)
+
+@login_manager.user_loader
+def load_user(id):
+    return User.find_by_id(id)
