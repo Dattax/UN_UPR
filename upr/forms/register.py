fc1c0b0889b3deda3952294caecd1d02434d7e98
sorressean <tyler@tysdomain.com>
2016-09-18 16:42:21 -0400

    ADded registration logic.

---
+++ upr/forms/register.py
@@ -0,0 +1,18 @@
+"""
+Contains user registration
+"""
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SelectField
+from wtforms.validators import DataRequired, Email
+
+class RegistrationForm(Form):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    first_name = StringField('FName')
+    last_name = StringField('LName')
+    phone = StringField('Phone')
+    address = StringField('Address')
+    postal_code = StringField('PostalCode')
+    province = StringField('Province')
+    country = SelectField('Country')
+    
