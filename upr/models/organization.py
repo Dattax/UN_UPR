1413cf06e8ee7b0dd127dc7802eb25c5fdd42c39
Harry Staley <staleyh@gmail.com>
2016-09-19 04:20:30 -0500

    CHORE: revised to conform to PEP8

--- upr/models/organization.py
+++ upr/models/organization.py
@@ -3,19 +3,19 @@ Contains the organization model.
 """
 from upr import db
 
+
 class Organization(db.Model):
     """
     The organization model provides access to organizations.
     Organizations are a collection of users belonging to a group.
     """
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable = False)
+    name = db.Column(db.String, nullable=False)
     acronym = db.Column(db.String)
-    org_type = db.Column(db.String, nullable = False)
-    accredited = db.Column(db.Boolean, default = False)
-    address = db.Column(db.String, nullable = False)
-    country = db.Column(db.String(32), nullable = False)
+    org_type = db.Column(db.String, nullable=False)
+    accredited = db.Column(db.Boolean, default=False)
+    address = db.Column(db.String, nullable=False)
+    country = db.Column(db.String(32), nullable=False)
     country_code = db.Column(db.String(6))
     phone = db.Column(db.String)
     web = db.Column(db.String)
-    
