2a8d7299d04ee15f9269d1d427b806c17e16c92d
Harry Staley <staleyh@gmail.com>
2017-01-08 21:04:09 -0600

    FEAT: Updated shell script for Native setup in Vagrand and renamed the file to correct spelling. draft = true

---
+++ requirements_shell.sh
@@ -0,0 +1,21 @@
+apt-get -qqy update
+apt-get install python-virtualenv
+apt-get install python-setuptools
+apt-get -qqy install postgresql python-psycopg2
+apt-get -qqy install python-flask python-sqlalchemy
+apt-get -qqy install python-pip
+pip install bleach
+pip install oauth2client
+pip install requests
+pip install httplib2
+pip install redis
+pip install passlib
+pip install itsdangerous
+pip install flask-httpauth
+pip install bcrypt
+pip install flask-sqlalchemy
+pip install flask-login
+pip install flask-wtf
+pip install uwsgi
+su postgres -c 'createuser -dRS vagrant'
+su vagrant -c 'createdb'
